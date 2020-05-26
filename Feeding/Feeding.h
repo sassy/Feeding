450a429b3368cb801009c9d831fb9094fd6b7d22
Satoshi Watanabe <sassy.watanabe@gmail.com>
2020-05-26 23:47:32 +0900

    Initial Commit

---
+++ Feeding/Feeding.h
@@ -0,0 +1,19 @@
+//
+//  Feeding.h
+//  Feeding
+//
+//  Created by Satoshi Watanabe on 2020/05/26.
+//  Copyright © 2020 Satoshi Watanabe. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Feeding.
+FOUNDATION_EXPORT double FeedingVersionNumber;
+
+//! Project version string for Feeding.
+FOUNDATION_EXPORT const unsigned char FeedingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Feeding/PublicHeader.h>
+
+
